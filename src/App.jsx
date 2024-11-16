fa454cfa7f8590dfb0aa41a0619d8dab819484f0
masud467 <masudpfrnt@gmail.com>
2024-11-16 23:17:42 +0600

    setup basic structure of the project.

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1 className="text-4xl bg-yellow-300 text-green-900">FlareLink PVT LTD Internship task </h1>
+    </>
+  );
+}
+
+export default App;
